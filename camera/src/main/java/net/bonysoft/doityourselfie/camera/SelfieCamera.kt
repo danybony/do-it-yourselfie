d2ec5339fee1380c6aae5ffb6591d580e926add9
Daniele Bonaldo <dany.bony@gmail.com>
2018-07-16 11:48:13 +0200

    First SelfieCamera implementation

---
+++ camera/src/main/java/net/bonysoft/doityourselfie/camera/SelfieCamera.kt
@@ -0,0 +1,219 @@
+package net.bonysoft.doityourselfie.camera
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Context.CAMERA_SERVICE
+import android.graphics.ImageFormat
+import android.hardware.camera2.CameraAccessException
+import android.hardware.camera2.CameraCaptureSession
+import android.hardware.camera2.CameraCharacteristics
+import android.hardware.camera2.CameraDevice
+import android.hardware.camera2.CameraManager
+import android.hardware.camera2.CaptureRequest
+import android.hardware.camera2.CaptureResult
+import android.hardware.camera2.TotalCaptureResult
+import android.media.ImageReader
+import android.os.Handler
+import android.view.Surface
+import timber.log.Timber
+
+private const val IMAGE_WIDTH = 3280
+private const val IMAGE_HEIGHT = 2464
+private const val MAX_IMAGES = 1
+
+class SelfieCamera
+private constructor() {
+
+    private var cameraDevice: CameraDevice? = null
+    private var captureSession: CameraCaptureSession? = null
+    private var imageReader: ImageReader? = null
+
+    private val stateCallback = object : CameraDevice.StateCallback() {
+        override fun onOpened(cameraDevice: CameraDevice) {
+            Timber.d("Opened camera.")
+            this@SelfieCamera.cameraDevice = cameraDevice
+        }
+
+        override fun onDisconnected(cameraDevice: CameraDevice) {
+            Timber.d("Camera disconnected, closing.")
+            cameraDevice.close()
+        }
+
+        override fun onError(cameraDevice: CameraDevice, i: Int) {
+            Timber.d("Camera device error, closing.")
+            cameraDevice.close()
+        }
+
+        override fun onClosed(cameraDevice: CameraDevice) {
+            Timber.d("Closed camera, releasing")
+            this@SelfieCamera.cameraDevice = null
+        }
+    }
+
+
+    private val mSessionCallback = object : CameraCaptureSession.StateCallback() {
+        override fun onConfigured(cameraCaptureSession: CameraCaptureSession) {
+            // The camera is already closed
+            if (cameraDevice == null) {
+                return
+            }
+
+            // When the session is ready, we start capture.
+            captureSession = cameraCaptureSession
+            triggerImageCapture()
+        }
+
+        override fun onConfigureFailed(cameraCaptureSession: CameraCaptureSession) {
+            Timber.e("Failed to configure camera")
+        }
+    }
+
+    private val mCaptureCallback = object : CameraCaptureSession.CaptureCallback() {
+
+        override fun onCaptureProgressed(session: CameraCaptureSession,
+                                         request: CaptureRequest,
+                                         partialResult: CaptureResult) {
+            Timber.d("Partial result")
+        }
+
+        override fun onCaptureCompleted(session: CameraCaptureSession,
+                                        request: CaptureRequest,
+                                        result: TotalCaptureResult) {
+            if (session != null) {
+                session.close()
+                captureSession = null
+                Timber.d("CaptureSession closed")
+            }
+        }
+    }
+
+    private object InstanceHolder {
+        internal val camera = SelfieCamera()
+    }
+
+    @SuppressLint("MissingPermission")
+    fun initializeCamera(context: Context,
+                         backgroundHandler: Handler,
+                         imageAvailableListener: ImageReader.OnImageAvailableListener) {
+        // Discover the camera instance
+        val manager = context.getSystemService(CAMERA_SERVICE) as CameraManager
+        var camIds = arrayOf<String>()
+        try {
+            camIds = manager.cameraIdList
+        } catch (e: CameraAccessException) {
+            Timber.e(e, "Cam access exception getting IDs")
+        }
+
+        if (camIds.isEmpty()) {
+            Timber.e("No cameras found")
+            return
+        }
+        val id = camIds[0]
+        Timber.d("Using camera id $id")
+
+
+        // Initialize the image processor
+        imageReader = ImageReader.newInstance(IMAGE_WIDTH, IMAGE_HEIGHT, ImageFormat.JPEG, MAX_IMAGES)
+        imageReader!!.setOnImageAvailableListener(imageAvailableListener, backgroundHandler)
+
+        // Open the camera resource
+        try {
+            manager.openCamera(id, stateCallback, backgroundHandler)
+        } catch (cae: CameraAccessException) {
+            Timber.d(cae, "Camera access exception")
+        }
+
+    }
+
+    fun takePicture() {
+        if (cameraDevice == null) {
+            Timber.e("Cannot capture image. Camera not initialized.")
+            return
+        }
+
+        // Here, we create a CameraCaptureSession for capturing still images.
+        try {
+            cameraDevice!!.createCaptureSession(
+                listOf<Surface>(imageReader!!.surface),
+                mSessionCallback, null)
+        } catch (cae: CameraAccessException) {
+            Timber.e(cae, "access exception while preparing pic")
+        }
+
+    }
+
+    private fun triggerImageCapture() {
+        try {
+            val captureBuilder = cameraDevice!!.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE)
+            captureBuilder.addTarget(imageReader!!.surface)
+            captureBuilder.set(CaptureRequest.CONTROL_AE_MODE, CaptureRequest.CONTROL_AE_MODE_ON)
+            captureBuilder.set(CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_AUTO)
+            captureBuilder.set(CaptureRequest.CONTROL_AWB_MODE, CaptureRequest.CONTROL_AWB_MODE_AUTO)
+            Timber.d("Session initialized.")
+            captureSession!!.capture(captureBuilder.build(), mCaptureCallback, null)
+        } catch (cae: CameraAccessException) {
+            Timber.e(cae, "camera capture exception")
+        }
+
+    }
+
+    /**
+     * Close the camera resources
+     */
+    fun shutDown() {
+        cameraDevice?.close()
+    }
+
+    companion object {
+
+        val instance: SelfieCamera
+            get() = InstanceHolder.camera
+
+        /**
+         * Helpful debugging method:  Dump all supported camera formats to log.  You don't need to run
+         * this for normal operation, but it's very helpful when porting this code to different
+         * hardware.
+         */
+        fun dumpFormatInfo(context: Context) {
+            val manager = context.getSystemService(CAMERA_SERVICE) as CameraManager
+            var camIds = arrayOf<String>()
+            try {
+                camIds = manager.cameraIdList
+            } catch (e: CameraAccessException) {
+                Timber.d("Cam access exception getting IDs")
+            }
+
+            if (camIds.isEmpty()) {
+                Timber.d("No cameras found")
+            }
+            for (camId in camIds) {
+                Timber.d("Using camera id $camId")
+                try {
+                    val characteristics = manager.getCameraCharacteristics(camId)
+                    val configs = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)
+                    for (format in configs!!.outputFormats) {
+                        Timber.d("Getting sizes for format: $format")
+                        for (s in configs.getOutputSizes(format)) {
+                            Timber.d("	$s")
+                        }
+                    }
+                    dumpAvailableCharacteristic(characteristics, CameraCharacteristics.CONTROL_AVAILABLE_EFFECTS, "Effect available: ")
+                    dumpAvailableCharacteristic(characteristics, CameraCharacteristics.CONTROL_AF_AVAILABLE_MODES, "AF Modes available: ")
+                    dumpAvailableCharacteristic(characteristics, CameraCharacteristics.CONTROL_AE_AVAILABLE_MODES, "AE Modes available: ")
+                    dumpAvailableCharacteristic(characteristics, CameraCharacteristics.CONTROL_AVAILABLE_SCENE_MODES, "Scene Modes available: ")
+                    dumpAvailableCharacteristic(characteristics, CameraCharacteristics.CONTROL_AWB_AVAILABLE_MODES, "AWB Modes available: ")
+                } catch (e: CameraAccessException) {
+                    Timber.d("Cam access exception getting characteristics.")
+                }
+
+            }
+        }
+
+        private fun dumpAvailableCharacteristic(characteristics: CameraCharacteristics, controlAfAvailableModes: CameraCharacteristics.Key<IntArray>, s2: String) {
+            val selectedCharacteristics = characteristics.get(controlAfAvailableModes) ?: return
+            for (characteristic in selectedCharacteristics) {
+                Timber.d("$s2$characteristic")
+            }
+        }
+    }
+}
--- things/src/main/java/net/bonysoft/doityourselfie/MainActivity.kt
+++ things/src/main/java/net/bonysoft/doityourselfie/MainActivity.kt
@@ -1,32 +1,88 @@
 package net.bonysoft.doityourselfie
 
 import android.app.Activity
+import android.media.ImageReader
 import android.os.Bundle
+import android.os.Environment
+import android.os.Handler
+import android.os.HandlerThread
+import net.bonysoft.doityourselfie.camera.SelfieCamera
+import timber.log.Timber
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.Date
 
-/**
- * Skeleton of an Android Things activity.
- *
- * Android Things peripheral APIs are accessible through the class
- * PeripheralManagerService. For example, the snippet below will open a GPIO pin and
- * set it to HIGH:
- *
- * <pre>{@code
- * val service = PeripheralManagerService()
- * val mLedGpio = service.openGpio("BCM6")
- * mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW)
- * mLedGpio.value = true
- * }</pre>
- * <p>
- * For more complex peripherals, look for an existing user-space driver, or implement one if none
- * is available.
- *
- * @see <a href="https://github.com/androidthings/contrib-drivers#readme">https://github.com/androidthings/contrib-drivers#readme</a>
- *
- */
 class MainActivity : Activity() {
+
+    private lateinit var camera: SelfieCamera
+    private lateinit var cameraThread: HandlerThread
+    private lateinit var cameraHandler: Handler
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (BuildConfig.DEBUG) SelfieCamera.dumpFormatInfo(this)
+
+        cameraThread = HandlerThread("CameraBackgroundThread")
+        cameraThread.start()
+        cameraHandler = Handler(cameraThread.looper)
+
+        camera = SelfieCamera.instance
+        camera.initializeCamera(this, cameraHandler, ImageReader.OnImageAvailableListener {
+            val image = it.acquireLatestImage()
+            val imageBuf = image.planes[0].buffer
+            val imageBytes = ByteArray(imageBuf.remaining())
+            imageBuf.get(imageBytes)
+            image.close()
+
+            onPictureTaken(imageBytes)
+        })
+    }
+
+    override fun onStart() {
+        super.onStart()
+        camera.takePicture()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        camera.shutDown()
+        cameraThread.quitSafely()
+    }
+
+    private fun onPictureTaken(imageBytes: ByteArray?) {
+        if (imageBytes != null) {
+            val pictureFile = getOutputMediaFile() ?: return
+            Timber.d("Storing image to ${pictureFile.absolutePath}")
+            try {
+                val fos = FileOutputStream(pictureFile)
+                fos.write(imageBytes)
+                fos.close()
+            } catch (e: IOException) {
+                Timber.e(e, "failed to store to file")
+            }
+        }
+    }
+
+    private fun getOutputMediaFile(): File? {
+        val mediaStorageDir = File(
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+            "MyCameraApp"
+        )
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Timber.d("failed to create directory")
+                return null
+            }
+        }
+        // Create a media file name
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val mediaFile: File
+        mediaFile = File(mediaStorageDir.path + File.separator + "IMG_" + timeStamp + ".jpg")
+
+        return mediaFile
     }
 }
