1da3c8cf0faaeab9d95f2b0532e4692e8088de6b
Daniele Bonaldo <dany.bony@gmail.com>
2018-07-18 16:20:01 +0200

    WIP multi picture capture

--- things/src/main/java/net/bonysoft/doityourselfie/MainActivity.kt
+++ things/src/main/java/net/bonysoft/doityourselfie/MainActivity.kt
@@ -1,10 +1,12 @@
 package net.bonysoft.doityourselfie
 
 import android.app.Activity
+import android.graphics.BitmapFactory
 import android.media.ImageReader
 import android.os.Bundle
 import android.os.Handler
 import android.os.HandlerThread
+import android.widget.ImageView
 import net.bonysoft.doityourselfie.camera.SelfieCamera
 import net.bonysoft.doityourselfie.camera.dumpFormatInfo
 
@@ -13,10 +15,16 @@ class MainActivity : Activity() {
     private lateinit var camera: SelfieCamera
     private lateinit var cameraThread: HandlerThread
     private lateinit var cameraHandler: Handler
+    private val imageViews = ArrayList<ImageView>()
+    private var nextImageId = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        imageViews.add(findViewById(R.id.imageTopLeft))
+        imageViews.add(findViewById(R.id.imageTopRight))
+        imageViews.add(findViewById(R.id.imageBottomLeft))
+        imageViews.add(findViewById(R.id.imageBottomRight))
 
         if (BuildConfig.DEBUG) dumpFormatInfo(this)
 
@@ -38,6 +46,10 @@ class MainActivity : Activity() {
 
     override fun onStart() {
         super.onStart()
+        startShootingProcess() // TODO do it on button clicked
+    }
+
+    private fun startShootingProcess() {
         camera.takePicture()
     }
 
@@ -48,8 +60,23 @@ class MainActivity : Activity() {
     }
 
     private fun onPictureTaken(imageBytes: ByteArray?) {
-        if (imageBytes != null) {
+        if (imageBytes == null) {
+            return
+        }
+        Thread {
             saveImageToFile(imageBytes)
+        }.start()
+
+        val bitmap = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes!!.size) // TODO resize depending on the view
+        runOnUiThread {
+            imageViews[nextImageId].setImageBitmap(bitmap)
+            nextImageId++
+            if (nextImageId >= 4) {
+                nextImageId = 0
+            } else {
+                Thread.sleep(3000)
+                camera.takePicture()
+            }
         }
     }
 
