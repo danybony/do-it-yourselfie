c86558ffbe5beb7aba75d97e55c22243b1dd2dcb
Roberto Orgiu <roberto.orgiu@gmail.com>
2018-07-22 18:22:34 -0400

    Add permission to get external storage

--- mobile/src/main/java/net/bonysoft/doityourselfie/PhotoLoadingActivity.kt
+++ mobile/src/main/java/net/bonysoft/doityourselfie/PhotoLoadingActivity.kt
@@ -1,10 +1,13 @@
 package net.bonysoft.doityourselfie
 
+import `in`.mayanknagwanshi.imagepicker.imagePicker.ImagePicker
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
 import android.content.Intent
+import android.content.pm.PackageManager.PERMISSION_GRANTED
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
-
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat.checkSelfPermission
+import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_photo_loading.*
 import net.bonysoft.doityourselfie.photos.model.CompleteAlbum
 
@@ -12,24 +15,60 @@ class PhotoLoadingActivity : AppCompatActivity() {
 
     companion object {
         private const val ALBUM_KEY = "net.bonysoft.doityourselfie.ALBUM"
+        private const val STORAGE_REQUEST_CODE = 1337
 
         @JvmStatic
-        fun showAlbumDetails(host: AppCompatActivity, album: CompleteAlbum) =
-                Intent(host, PhotoLoadingActivity::class.java).apply {
-                    putExtra(ALBUM_KEY, album)
-                }
+        fun showAlbumDetails(host: AppCompatActivity, album: CompleteAlbum) {
+            host.startActivity(
+                    Intent(host, PhotoLoadingActivity::class.java).apply {
+                        putExtra(ALBUM_KEY, album)
+                    }
+            )
+        }
     }
+
+    private val picker = ImagePicker()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo_loading)
         setSupportActionBar(toolbar)
 
-        addPicture.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        addPicture.setOnClickListener {
+            loadPicturePicker()
         }
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
+    private fun loadPicturePicker() {
+        if (checkSelfPermission(this, READ_EXTERNAL_STORAGE) == PERMISSION_GRANTED) {
+            startPicker()
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    arrayOf(READ_EXTERNAL_STORAGE), STORAGE_REQUEST_CODE)
+        }
+    }
+
+    private fun startPicker() {
+        picker.withActivity(this)
+                .chooseFromGallery(true)
+                .chooseFromCamera(false)
+                .start()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == STORAGE_REQUEST_CODE) {
+            checkPermissions(permissions, grantResults)
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+    }
+
+    private fun checkPermissions(permissions: Array<out String>, grantResults: IntArray) {
+        permissions.forEachIndexed { index, permission ->
+            if (permission == READ_EXTERNAL_STORAGE && grantResults[index] == PERMISSION_GRANTED) {
+                startPicker()
+            }
+        }
+    }
 }
