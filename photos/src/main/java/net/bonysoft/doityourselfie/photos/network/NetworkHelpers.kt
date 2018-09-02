ab029aacd8e010743c1529366e782ef176b0c423
Roberto Orgiu <roberto.orgiu@gmail.com>
2018-09-02 14:17:44 +0200

    Extract function to create Retrofit in order to have it work properly for tests

---
+++ photos/src/main/java/net/bonysoft/doityourselfie/photos/network/NetworkHelpers.kt
@@ -0,0 +1,24 @@
+package net.bonysoft.doityourselfie.photos.network
+
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.experimental.CoroutineCallAdapterFactory
+import com.squareup.moshi.Moshi
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+internal const val API_ENDPOINT = "https://photoslibrary.googleapis.com/v1/"
+internal const val OCTET_TYPE = "application/octet-stream"
+internal const val AUTHORIZATION = "Authorization"
+internal const val UPLOAD_FILENAME = "X-Goog-Upload-File-Name"
+
+internal fun createRetrofit(moshi: Moshi,
+                            client: OkHttpClient,
+                            endpoint: String): ApiService =
+        Retrofit.Builder()
+                .addConverterFactory(MoshiConverterFactory.create(moshi))
+                .addCallAdapterFactory(CoroutineCallAdapterFactory())
+                .baseUrl(endpoint)
+                .client(client)
+                .build().let {
+                    it.create(ApiService::class.java)
+                }
